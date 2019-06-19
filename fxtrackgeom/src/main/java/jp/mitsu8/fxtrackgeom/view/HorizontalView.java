0d12b68a69bca2536bbf425085d013d13b1e06bc
8-mitsu <mitsu8_yh@yahoo.co.jp>
2019-06-19 17:18:03 +0900

    [WIP] HorizontalView

--- fxtrackgeom/src/main/java/jp/mitsu8/fxtrackgeom/view/HorizontalView.java
+++ fxtrackgeom/src/main/java/jp/mitsu8/fxtrackgeom/view/HorizontalView.java
@@ -1,15 +1,20 @@
 package jp.mitsu8.fxtrackgeom.view;
 
+import javafx.beans.binding.Bindings;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
 import javafx.collections.ListChangeListener.Change;
 import javafx.collections.ObservableList;
+import javafx.collections.ObservableListBase;
 import javafx.collections.transformation.TransformationList;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.layout.Pane;
+import javafx.scene.shape.Path;
 import javafx.scene.shape.PathElement;
+import javafx.util.Callback;
 import javafx.util.Pair;
 import jp.mitsu8.fxtrackgeom.horizontal.HorizontalElement;
 import jp.mitsu8.fxtrackgeom.horizontal.HorizontalLayout;
@@ -23,6 +28,26 @@ public class HorizontalView extends Pane {
 		return layouts;
 	}
 	
-	
+	private void TEMP_addlisteners() {
+		getLayouts().addListener((ListChangeListener.Change<? extends HorizontalLayout> c) -> {
+			while(c.next()) {
+				if (c.wasPermutated()) {
+					for (int i = c.getFrom(); i < c.getTo(); i++) {
+						
+					}
+				}
+			}
+		});
+		Callback<ObservableList<PathElement>, Path> pathFactory = param -> {
+			Path p = new Path();
+			Bindings.bindContent(p.getElements(), param);
+			return p;
+		};
+		Callback<ObservableList<Path>, Pane> paneFactory = param -> {
+			Pane p = new Pane();
+			Bindings.bindContent(p.getChildren(), param);
+			return p;
+		};
+	}
 	
 }
