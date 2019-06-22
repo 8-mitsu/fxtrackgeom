79f036b1f1d26d8297e8ea698da68161ab2474bb
8-mitsu <mitsu8_yh@yahoo.co.jp>
2019-06-23 00:59:29 +0900

    [WIP] HorizontalView

--- fxtrackgeom/src/main/java/jp/mitsu8/fxtrackgeom/view/HorizontalView.java
+++ fxtrackgeom/src/main/java/jp/mitsu8/fxtrackgeom/view/HorizontalView.java
@@ -1,53 +1,37 @@
 package jp.mitsu8.fxtrackgeom.view;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javafx.beans.binding.Bindings;
-import javafx.beans.property.ObjectProperty;
-import javafx.beans.property.SimpleObjectProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ListChangeListener;
-import javafx.collections.ListChangeListener.Change;
 import javafx.collections.ObservableList;
-import javafx.collections.ObservableListBase;
-import javafx.collections.transformation.TransformationList;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.layout.Pane;
 import javafx.scene.shape.Path;
-import javafx.scene.shape.PathElement;
-import javafx.util.Callback;
-import javafx.util.Pair;
 import jp.mitsu8.fxtrackgeom.horizontal.HorizontalElement;
-import jp.mitsu8.fxtrackgeom.horizontal.HorizontalLayout;
-import jp.mitsu8.fxtrackgeom.horizontal.HorizontalLayout.ElementOrientation;
 
 public class HorizontalView extends Pane {
 	
-	private final ObservableList<HorizontalLayout> layouts = FXCollections.observableArrayList();
+	private final ObservableList<HorizontalElement> elements = FXCollections.observableArrayList();
 	
-	public final ObservableList<HorizontalLayout> getLayouts() {
-		return layouts;
+	public final ObservableList<HorizontalElement> getElements() {
+		return elements;
 	}
 	
-	private void TEMP_addlisteners() {
-		getLayouts().addListener((ListChangeListener.Change<? extends HorizontalLayout> c) -> {
-			while(c.next()) {
-				if (c.wasPermutated()) {
-					for (int i = c.getFrom(); i < c.getTo(); i++) {
-						
-					}
+	{
+		final Map<HorizontalElement, Path> paths = new HashMap<>();
+		getElements().addListener((ListChangeListener.Change<? extends HorizontalElement> c) -> {
+			while (c.next()) {
+				for (HorizontalElement el : c.getRemoved())
+					getChildren().remove(paths.remove(el));
+				for (HorizontalElement el : c.getAddedSubList()) {
+					Path path = new Path();
+					Bindings.bindContent(path.getElements(), el.getPath());
+					getChildren().add(paths.put(el, path));
 				}
 			}
 		});
-		Callback<ObservableList<PathElement>, Path> pathFactory = param -> {
-			Path p = new Path();
-			Bindings.bindContent(p.getElements(), param);
-			return p;
-		};
-		Callback<ObservableList<Path>, Pane> paneFactory = param -> {
-			Pane p = new Pane();
-			Bindings.bindContent(p.getChildren(), param);
-			return p;
-		};
 	}
 	
 }
