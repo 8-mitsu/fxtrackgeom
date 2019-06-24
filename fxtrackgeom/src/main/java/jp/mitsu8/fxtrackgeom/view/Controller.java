2149bb042c1c7afc5ac2707a19c54e1752052ff4
8-mitsu <mitsu8_yh@yahoo.co.jp>
2019-06-25 07:39:25 +0900

    added Commons-Collections to dependencies

--- fxtrackgeom/src/main/java/jp/mitsu8/fxtrackgeom/view/Controller.java
+++ fxtrackgeom/src/main/java/jp/mitsu8/fxtrackgeom/view/Controller.java
@@ -3,7 +3,6 @@ package jp.mitsu8.fxtrackgeom.view;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-import javafx.beans.property.ObjectProperty;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TreeView;
@@ -14,7 +13,7 @@ public class Controller implements Initializable {
 	
 	@FXML private TreeView<String> hierarchy;
 	
-	private ObjectProperty<Layout> layout;
+	private Layout layout;
 	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
--- fxtrackgeom/src/main/java/jp/mitsu8/fxtrackgeom/view/LayoutHierarchy.java
+++ fxtrackgeom/src/main/java/jp/mitsu8/fxtrackgeom/view/LayoutHierarchy.java
@@ -1,11 +1,19 @@
 package jp.mitsu8.fxtrackgeom.view;
 
-
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
+import javafx.collections.ListChangeListener;
+import javafx.collections.ObservableList;
+import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
 import javafx.scene.layout.VBox;
+
+import org.apache.commons.collections4.BidiMap;
+import org.apache.commons.collections4.bidimap.DualHashBidiMap;
+
+
 import jp.mitsu8.fxtrackgeom.Layout;
+import jp.mitsu8.fxtrackgeom.horizontal.HorizontalElement;
 
 public class LayoutHierarchy extends VBox {
 	
@@ -19,7 +27,49 @@ public class LayoutHierarchy extends VBox {
 	
 	
 	public final ObjectProperty<Layout> layoutProperty() {
-		return layout == null ? layout = new SimpleObjectProperty<>(this, "layout", null) : layout;
+		if (layout == null) {
+			layout = new SimpleObjectProperty<>(this, "layout");
+			layout.addListener((observable, oldValue, newValue) -> {
+				if (newValue == null)
+					treeView.setRoot(null);
+				else {
+					final TreeItem<String> root = new TreeItem<>();
+					root.valueProperty().bindBidirectional(newValue.nameProperty());
+					
+					ListChangeListener<Object> listListener = new ListChangeListener<Object>() {
+						
+						final ObservableList<HorizontalElement> elements = newValue.getElements();
+						final ObservableList<TreeItem<String>> items = root.getChildren();
+						
+						final BidiMap<HorizontalElement, TreeItem<String>> map = new DualHashBidiMap<>();
+
+						@Override
+						public void onChanged(Change<? extends Object> c) {
+							if (c.getList() == elements) {
+								while (c.next()) {
+									if (c.wasPermutated()) {
+										for (int i = c.getFrom(); i < c.getTo(); i++) {
+											int p = c.getPermutation(i);
+											items.set(p, map.get(elements.get(p)));
+										}
+									} else {
+										if (c.wasRemoved()) {
+											for (int i = c.getFrom(); i < c.getTo(); i++) {
+//												int p = c.getPermutation(i);
+//												items.set(p, map.get(elements.get(p)));
+											}
+										}
+									}
+								}
+							}
+						}
+						
+					};
+					
+				}
+			});
+		}
+		return layout;
 	}
 	
 	public Layout getLayout() {
