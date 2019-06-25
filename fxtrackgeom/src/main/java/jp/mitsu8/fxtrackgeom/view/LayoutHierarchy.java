806d6acd8537302e3871ec88ca94affd07a028a5
8-mitsu <mitsu8_yh@yahoo.co.jp>
2019-06-25 20:00:41 +0900

    [WIP] LayoutHierarchy

--- fxtrackgeom/src/main/java/jp/mitsu8/fxtrackgeom/view/LayoutHierarchy.java
+++ fxtrackgeom/src/main/java/jp/mitsu8/fxtrackgeom/view/LayoutHierarchy.java
@@ -7,17 +7,19 @@ import javafx.collections.ObservableList;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
 import javafx.scene.layout.VBox;
+import javafx.util.Pair;
 
 import org.apache.commons.collections4.BidiMap;
 import org.apache.commons.collections4.bidimap.DualHashBidiMap;
 
 
 import jp.mitsu8.fxtrackgeom.Layout;
+import jp.mitsu8.fxtrackgeom.horizontal.Group;
 import jp.mitsu8.fxtrackgeom.horizontal.HorizontalElement;
 
 public class LayoutHierarchy extends VBox {
 	
-	private TreeView<String> treeView;
+	private TreeView<HorizontalElement> treeView;
 	private ObjectProperty<Layout> layout;
 	
 	public LayoutHierarchy() {
@@ -33,44 +35,74 @@ public class LayoutHierarchy extends VBox {
 				if (newValue == null)
 					treeView.setRoot(null);
 				else {
-					final TreeItem<String> root = new TreeItem<>();
-					root.valueProperty().bindBidirectional(newValue.nameProperty());
+					final TreeItem<HorizontalElement> root = new TreeItem<>();
 					
-					ListChangeListener<Object> listListener = new ListChangeListener<Object>() {
-						
-						final ObservableList<HorizontalElement> elements = newValue.getElements();
-						final ObservableList<TreeItem<String>> items = root.getChildren();
-						
-						final BidiMap<HorizontalElement, TreeItem<String>> map = new DualHashBidiMap<>();
-
-						@Override
-						public void onChanged(Change<? extends Object> c) {
-							if (c.getList() == elements) {
-								while (c.next()) {
-									if (c.wasPermutated()) {
-										for (int i = c.getFrom(); i < c.getTo(); i++) {
-											int p = c.getPermutation(i);
-											items.set(p, map.get(elements.get(p)));
-										}
-									} else {
-										if (c.wasRemoved()) {
-											for (int i = c.getFrom(); i < c.getTo(); i++) {
-//												int p = c.getPermutation(i);
-//												items.set(p, map.get(elements.get(p)));
-											}
-										}
-									}
-								}
-							}
-						}
-						
-					};
+					createHierarchy(newValue.getElements(), root);
 					
 				}
 			});
 		}
 		return layout;
 	}
+	
+	private void createHierarchy(final ObservableList<HorizontalElement> elements, final TreeItem<HorizontalElement> root) {
+		ListChangeListener<Object> listListener = new ListChangeListener<Object>() {
+			
+			final ObservableList<TreeItem<HorizontalElement>> items = root.getChildren();
+			
+			final BidiMap<HorizontalElement, TreeItem<HorizontalElement>> map = new DualHashBidiMap<>();
+			
+			@Override
+			public void onChanged(Change<? extends Object> c) {
+				if (c.getList() == elements) {
+					while (c.next()) {
+						if (c.wasPermutated()) {
+							for (int i = c.getFrom(); i < c.getTo(); i++) {
+								int p = c.getPermutation(i);
+								items.set(p, map.get(elements.get(p)));
+							}
+						} else {
+							if (c.wasRemoved()) {
+								if (c.wasAdded()) {
+									for (int i = c.getFrom(); i < c.getTo(); i++) {
+										HorizontalElement element = elements.get(i);
+										if (!map.containsKey(element)) {
+											TreeItem<HorizontalElement> item = new TreeItem<>();
+											item.setValue(element);
+											if (element instanceof Group)
+												createHierarchy(((Group) element).getElements(), item);
+											map.put(element, item);
+										}
+										items.set(i, map.get(elements.get(i)));
+									}
+								} else {
+									for (Object removed : c.getRemoved()) {
+										items.remove(map.get(removed));
+									}
+								}
+							} else if (c.wasAdded()) {
+								for (int i = c.getTo() - 1; i >= c.getFrom(); i--) {
+									HorizontalElement element = elements.get(i);
+									if (!map.containsKey(element)) {
+										TreeItem<HorizontalElement> item = new TreeItem<>();
+										item.setValue(element);
+										if (element instanceof Group)
+											createHierarchy(((Group) element).getElements(), item);
+										map.put(element, item);
+									}
+									items.set(i, map.get(elements.get(i)));
+								}
+							}
+						}
+					}
+				}
+			}
+			
+		};
+		root.getChildren().addListener(listListener);
+	}
+	
+	
 	
 	public Layout getLayout() {
 		return layout == null ? null : layout.get();
